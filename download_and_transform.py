180007aa1318e8d3785723ec962e0179452e334b
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-01-23 12:38:54 +0100

    adding filter for col

--- download_and_transform.py
+++ download_and_transform.py
@@ -28,24 +28,25 @@ ids = list(ids)
 ic(ids)
 ic(len(ids))
 
-for y in ids[:1]:
-    col_id = y
-    print(f"processing collection: {col_id}")
-    mpr_docs = transkribus_client.collection_to_mets(col_id, file_path="./mets")
-    ic(f"{METS_DIR}/{col_id}*.xml")
-    files = glob.glob(f"{METS_DIR}/{col_id}/*_mets.xml")
-    for x in files:
-        tail = os.path.split(x)[-1]
-        doc_id = tail.split("_")[0]
-        tei_file = f"{doc_id}.xml"
-        ic(f"transforming mets: {x} to {tei_file}")
-        with PySaxonProcessor(license=False) as proc:
-            xsltproc = proc.new_xslt30_processor()
-            xsltproc.set_parameter("combine", proc.make_boolean_value(True))
-            xsltproc.set_parameter("ab", proc.make_boolean_value(True))
-            document = proc.parse_xml(xml_file_name=x)
-            executable = xsltproc.compile_stylesheet(stylesheet_file=XSLT)
-            output = executable.transform_to_string(xdm_node=document)
-            output = output.replace(' type=""', "")
-            with open(os.path.join(TEI_DIR, tei_file), "w") as f:
-                f.write(output)
+for y in ids:
+    if y == "209213":
+        col_id = y
+        print(f"processing collection: {col_id}")
+        mpr_docs = transkribus_client.collection_to_mets(col_id, file_path="./mets")
+        ic(f"{METS_DIR}/{col_id}*.xml")
+        files = glob.glob(f"{METS_DIR}/{col_id}/*_mets.xml")
+        for x in files:
+            tail = os.path.split(x)[-1]
+            doc_id = tail.split("_")[0]
+            tei_file = f"{doc_id}.xml"
+            ic(f"transforming mets: {x} to {tei_file}")
+            with PySaxonProcessor(license=False) as proc:
+                xsltproc = proc.new_xslt30_processor()
+                xsltproc.set_parameter("combine", proc.make_boolean_value(True))
+                xsltproc.set_parameter("ab", proc.make_boolean_value(True))
+                document = proc.parse_xml(xml_file_name=x)
+                executable = xsltproc.compile_stylesheet(stylesheet_file=XSLT)
+                output = executable.transform_to_string(xdm_node=document)
+                output = output.replace(' type=""', "")
+                with open(os.path.join(TEI_DIR, tei_file), "w") as f:
+                    f.write(output)
